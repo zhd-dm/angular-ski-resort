f39663aec3d2dc77eae0f94de7310ba7f624d364
Demyan <dyo171717@gmail.com>
2022-07-03 00:36:14 +0300

    feat: Добавлен компонент новой кнопки

---
+++ src/app/models/UI/button-default/button-default.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-default',
+  templateUrl: './button-default.component.html',
+  styleUrls: ['./button-default.component.scss']
+})
+export class ButtonDefaultComponent {
+
+  btnValue = 'ОК';
+
+}
